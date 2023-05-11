98d01ae846164134ce9e818376201ade34f27268
Javan <javan.o.otieno@gmail.com>
2023-05-11 08:17:32 +0300

    Added tests for object creation from a dictionary

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,5 +1,5 @@
-'''Contains tests for the base model class.
-'''
+"""Contains tests for the base model class.
+"""
 from datetime import datetime
 from time import sleep
 from unittest import TestCase
@@ -8,19 +8,19 @@ from models.base_model import BaseModel
 
 
 class BaseModelTestCase(TestCase):
-    '''Test case for the base model class.
-    '''
+    """Test case for the base model class.
+    """
     def setUp(self):
-        '''Sets up the test case.
-        '''
+        """Sets up the test case.
+        """
         self.model = BaseModel()
 
     def test_class_has_correct_attributes(self):
-        '''Test the class has the attributes and they are of the correct type.
+        """Test the class has the attributes and they are of the correct type.
             - id (str):
             - created_at (datetime):
             - updated_at (datetime):
-        '''
+        """
         getattr(self.model, 'id')
         getattr(self.model, 'created_at')
         getattr(self.model, 'updated_at')
@@ -30,16 +30,16 @@ class BaseModelTestCase(TestCase):
         self.assertIsInstance(self.model.updated_at, datetime)
 
     def test_id_is_different_for_each_object(self):
-        '''Test the id attribute is set correctly and is unique.
-        '''
+        """Test the id attribute is set correctly and is unique.
+        """
         self.model = BaseModel()
         model2 = BaseModel()
 
         self.assertNotEqual(self.model.id, model2.id)
 
     def test_string_representation_of_the_object(self):
-        '''Test the __str__ returns the correct representation.
-        '''
+        """Test the __str__ returns the correct representation.
+        """
         string = str(self.model).split(" ")
 
         self.assertEqual(string[0], '[' + 'BaseModel' + ']')
@@ -47,8 +47,8 @@ class BaseModelTestCase(TestCase):
         self.assertEqual(string[2][0], '{')
 
     def test_the_save_method(self):
-        '''Test the save() method.
-        '''
+        """Test the save() method.
+        """
         old_updated_time = self.model.updated_at
         sleep(0.1)
         self.model.save()
@@ -56,15 +56,40 @@ class BaseModelTestCase(TestCase):
         self.assertNotEqual(old_updated_time, new_updated_time)
 
     def test_the_to_dict_method(self):
-        '''Test the to_dict() method.
-        '''
-        json = self.model.to_dict()
+        """Test the to_dict() method.
+        """
+        model_dict = self.model.to_dict()
 
-        self.assertIsInstance(json, dict)
-        self.assertIsNotNone(json.get('__class__'))
-        self.assertIsInstance(json.get('__class__'), str)
-        self.assertEqual(json.get('__class__'), 'BaseModel')
-        self.assertIsInstance(json.get('created_at'), str)
-        self.assertIsInstance(json.get('updated_at'), str)
-        self.assertIsInstance(json.get('id'), str)
-        self.assertEqual(json.get('created_at')[4], '-')
+        self.assertIsInstance(model_dict, dict)
+        self.assertIsNotNone(model_dict.get('__class__'))
+        self.assertIsInstance(model_dict.get('__class__'), str)
+        self.assertEqual(model_dict.get('__class__'), 'BaseModel')
+        self.assertIsInstance(model_dict.get('created_at'), str)
+        self.assertIsInstance(model_dict.get('updated_at'), str)
+        self.assertIsInstance(model_dict.get('id'), str)
+        self.assertEqual(model_dict.get('created_at')[4], '-')
+
+    def test_initialization_of_object_from_non_empty_dictionary(self):
+        """Test the initialization of the object from a non empty dictionary.
+        """
+        model_dict = {
+            "id": "fc0b88fd-6df9-4bed-b8fb-849ed48ed8c8",
+            "created_at": "2023-05-11T06:10:19.919328",
+            "updated_at": "2023-05-11T06:10:19.919337",
+            "__class__": "BaseModel"
+        }
+
+        model = BaseModel(model_dict)
+        getattr(model, 'id')
+        getattr(model, 'created_at')
+        getattr(model, 'updated_at')
+
+    def test_initialization_of_object_from_an_empty_dictionary(self):
+        """Test the initialization of the object from an empty dictionary.
+        """
+        model_dict = {}
+
+        model = BaseModel(model_dict)
+        getattr(model, 'id')
+        getattr(model, 'created_at')
+        getattr(model, 'updated_at')
