406c232801c31b75500e306971611c1d171c4cfd
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-05-12 09:50:24 +0300

    Changed save method.It had a bug

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,11 +5,12 @@ import json
 
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances:
+    """Serializes instances to a JSON file and
+        deserializes JSON file to instances:
         Private class attributes:
             __file_path: (str) - path to the JSON file
-            __objects: (dict) - empty but will store all objects by <class name>.id
-                                (ex: to store a BaseModel object with id=12121212,
+            __objects: (dict) - empty but will store all objects
+            by<class name>.id(ex: to store a BaseModel object with id=12121212,
                                 the key will be BaseModel.12121212)
         Public instance methods:
             all(self): returns the dictionary _objects
@@ -32,15 +33,16 @@ class FileStorage:
         """Sets in __objects the obj with key <class name>.id
         """
         if obj and obj.id:
-            key = type(obj).__name__ + '.' +  obj.id
+            key = type(obj).__name__ + '.' + obj.id
             self.__objects[key] = obj.to_dict()
 
     def save(self):
         """Serializes __objects to JSON file
         """
-        if self.__objects:
-            with open(self.__file_path, "w") as file:
-                json.dump(self.__objects, file)
+        objects = self.__objects
+        object_dict = {key: objects[key] for key in objects.keys()}
+        with open(self.__file_path, 'w') as file:
+            json.dump(object_dict, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects
