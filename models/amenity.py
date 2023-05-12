9994dfe505663a629c7c7a179b55523da3deabf0
Javan <javan.o.otieno@gmail.com>
2023-05-12 21:58:58 +0300

    create amenity.py
    - Implemented Amenity model class.

---
+++ models/amenity.py
@@ -0,0 +1,20 @@
+"""
+Supplies the Amenity Model class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """The Amenity model
+
+        Attributes:
+            name: (str) - empty string
+
+    """
+    name = ""
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return super().__str__()
