fe9976b49a94f929960d256b04b8b1f0470de150
Javan <javan.o.otieno@gmail.com>
2023-05-12 22:06:48 +0300

    create state.py
    - Implemented the state model class

---
+++ models/state.py
@@ -0,0 +1,19 @@
+"""
+Supplies the State Model class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """The State model
+
+        Attributes:
+            name: (str) - empty string
+    """
+    name = ""
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return super().__str__()
