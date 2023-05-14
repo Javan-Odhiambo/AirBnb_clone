8859510160c0188fbae46c4b011766634008d90b
Javan <javan.o.otieno@gmail.com>
2023-05-14 10:20:34 +0300

    Updated console test

--- tests/test_console.py
+++ tests/test_console.py
@@ -59,3 +59,44 @@ class HBNBCommandTestCase(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as output:
             self.assertFalse(self.console.onecmd(''))
             self.assertEqual(output.getvalue(), '')
+
+class HBNBCommandCreateTestCase(unittest.TestCase):
+    """Unittests for testing create from the HBNB command interpreter."""
+
+    @classmethod
+    def setUp(self):
+        self.console = HBNBCommand()
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+        FileStorage.__objects = {}
+
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    def test_missing_class(self):
+        message = "** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.console.onecmd("create"))
+            self.assertEqual(message, output.getvalue().strip())
+
+    def test_create_invalid_class(self):
+        message = "** class doesn't exist **"
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.console.onecmd("create MyModel"))
+            self.assertEqual(message, output.getvalue().strip())
+
+    def test_invalid_syntax(self):
+        message = "*** Unknown syntax: MyModel.create()"
+        with patch('sys.stdout', new=StringIO()) as output:
+            self.assertFalse(self.console.onecmd('MyModel.create()'))
+            # self.assertEqual(message, output.getvalue().strip())
