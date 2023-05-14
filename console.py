5192dd9b0898a3dd4d5030f17d7ffdbb4ef17514
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-05-14 12:27:47 +0300

    updated console.py

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 Defines the HBnB console.
 """
 import cmd
+import re
+from shlex import split
 from models import storage
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -11,6 +13,24 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+
+
+def parse_arguments(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 class HBNBCommand(cmd.Cmd):
@@ -47,6 +67,30 @@ class HBNBCommand(cmd.Cmd):
         class_name = instance["__class__"]
         return self.__classes[class_name](**instance)
 
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        command_dict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        dot_match = re.search(r"\.", arg)
+        if dot_match is not None:
+            arg_parts = [arg[:dot_match.start()], arg[dot_match.end():]]
+            parentheses_match = re.search(r"\((.*?)\)", arg_parts[1])
+            if parentheses_match is not None:
+                command_parts = [
+                    arg_parts[1][:parentheses_match.start()],
+                    parentheses_match.group()[1:-1]
+                ]
+                if command_parts[0] in command_dict:
+                    arguments = "{} {}".format(arg_parts[0], command_parts[1])
+                    return command_dict[command_parts[0]](arguments)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
+
     def do_quit(self, args):
         """Quit command to exit the program"""
         return True
@@ -71,8 +115,8 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-
-        class_name = args.split()[0]
+        args = parse_arguments(args)
+        class_name = args[0]
         if class_name not in self.__classes:
             print("** class doesn't exist **")
             return
@@ -94,7 +138,7 @@ class HBNBCommand(cmd.Cmd):
                 - Prints an error if the instance is not found.
         """
         object_dict = storage.all()
-        args = args.split()
+        args = parse_arguments(args)
 
         if len(args) == 0:
             print("** class name missing **")
@@ -109,7 +153,7 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
             return
 
-        id = args[1]
+        id = args[1].replace('"', '')
         key = "{}.{}".format(class_name, id)
         found = False
         for k, instance in object_dict.items():
@@ -145,7 +189,7 @@ class HBNBCommand(cmd.Cmd):
             return
 
         object_dict = storage.all()
-        id = args.split()[1]
+        id = args.split()[1].replace('"', '')
         key = "{}.{}".format(class_name, id)
         found = False
 
@@ -169,7 +213,8 @@ class HBNBCommand(cmd.Cmd):
             Description:
                 - Given a wrong class, It prints an error.
         """
-        class_name = args.split()[0] if args else None
+        args = parse_arguments(args)
+        class_name = args[0] if args else None
         object_dict = storage.all()
 
         if class_name and class_name not in self.__classes:
@@ -195,9 +240,9 @@ class HBNBCommand(cmd.Cmd):
                 - Prints different errors if given the wrong input.
                 - Prints an error if the instance is not stored.
         """
-        class_name = args.split()[0] if args else None
-        args = args.split()
-
+        args = parse_arguments(args)
+        class_name = args[0] if args else None
+        dictionary = True if type(eval(args[2])) is dict else False
         if class_name is None:
             print("** class name missing **")
             return
@@ -210,30 +255,43 @@ class HBNBCommand(cmd.Cmd):
         if len(args) < 3:
             print("** attribute name missing **")
             return
-        if len(args) < 4:
+        if len(args) < 4 and not dictionary:
             print("** value missing **")
             return
-
         instance_id = args[1]
-        attribute_name = args[2]
-        attribute_value = args[3].strip('"')
-
-        if attribute_value.isdigit():
-            print("Yes")
-            attribute_value = int(attribute_value)
-
-        object_dict = storage.all()
         obj_key = "{}.{}".format(class_name, instance_id)
+        object_dict = storage.all()
         if obj_key not in object_dict:
             print("** no instance found **")
             return
+        instance = object_dict[obj_key]
+        if dictionary:
+            the_dict = eval(args[2])
+            for k, val in the_dict.items():
+                if k in ["id", "created_at", "updated_at"]:
+                    return
+                instance[k] = val
+        else:
+            attribute_name = args[2]
+            attribute_value = args[3].strip('"')
+            if attribute_value.isdigit():
+                attribute_value = int(attribute_value)
+            if len(args) == 4:
+                if attribute_name in ["id", "created_at", "updated_at"]:
+                    return
+                instance[attribute_name] = attribute_value
+        storage.save()
 
-        if len(args) == 4:
-            instance = object_dict[obj_key]
-            if attribute_name in ["id", "created_at", "updated_at"]:
-                return
-            instance[attribute_name] = attribute_value
-            storage.save()
+    def do_count(self, args):
+        """Usage: count <class> or <class>.count()
+        Retrieve the number of instances of a given class."""
+        args = parse_arguments(args)
+        class_name = args[0]
+        count = 0
+        for instance in storage.all().values():
+            if instance['__class__'] == class_name:
+                count += 1
+        print(count)
 
 
 if __name__ == '__main__':
